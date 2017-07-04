bd43e9f9aebad2cfbf2178e378fc5332a4b09f80
Sidharth Narayanan <iamisidharth@gmail.com>
2017-07-04 06:44:44 +0530

    Now completely browserified
    No need of nodeJS

--- DF_Reader.js
+++ DF_Reader.js
@@ -1,4 +1,5 @@
-var buffer=require("fs").readFileSync('2.bin');
+var buffer;
+var data;
 var FMT=[];
 var offset = 0;
 var offsetArray=[];
@@ -7,9 +8,9 @@ var msgType=[];
 FMT[128]={'Type':'128','length':'89','Name':'FMT','Format':'BBnNZ','Columns':'Type,Length,Name,Format,Columns'};
 
 
- if (!Buffer.isBuffer(buffer)) {
-      throw new Error("argument buffer is not a Buffer object");
-  }
+ //if (!Buffer.isBuffer(buffer)) {
+      //throw new Error("argument buffer is not a Buffer object");
+  //}
 
 
 function FORMAT_TO_STRUCT(obj)
@@ -21,88 +22,90 @@ function FORMAT_TO_STRUCT(obj)
         temp=obj.Format.charAt(i);
         switch(temp){
             case 'b':
-                dict[column[i]]=buffer.readInt8(offset);
+                dict[column[i]]=data.getInt8(offset);
                 offset+=1;
                 break;
             case 'B':
-                dict[column[i]]=buffer.readUInt8(offset);
+                dict[column[i]]=data.getUint8(offset);
                 offset+=1;
                 break;
             case 'h':
-                dict[column[i]]=buffer.readInt16LE(offset);
+                dict[column[i]]=data.getInt16(offset,true);
                 offset+=2;
                 break;
             case 'H':
-                dict[column[i]]=buffer.readUInt16LE(offset);
+                dict[column[i]]=data.getUint16(offset,true);
                 offset+=2;
                 break;
             case 'i':
-                dict[column[i]]=buffer.readInt32LE(offset);
+                dict[column[i]]=data.getInt32(offset,true);
                 offset+=4;
                 break;
             case 'I':
-                dict[column[i]]=buffer.readUInt32LE(offset);
+                dict[column[i]]=data.getUint32(offset,true);
                 offset+=4;
                 break;
             case 'f':
-                dict[column[i]]=buffer.readFloatLE(offset);
+                dict[column[i]]=data.getFloat32(offset,true);
                 offset+=4;
                 break;
             case 'd':
-                dict[column[i]]=buffer.readDoubleLE(offset);
+                dict[column[i]]=data.getFloat64(offset,true);
                 offset+=8;
                 break;
             case 'Q':
-                var low = buffer.readUInt32LE(offset);
+                var low = data.getUint32(offset,true);
                 offset+=4;
-                var n = buffer.readUInt32LE(offset) * 4294967296.0 + low;
+                var n = data.getUint32(offset,true) * 4294967296.0 + low;
                 if (low < 0) n += 4294967296;
                 dict[column[i]]=n;
                 offset+=4;
                 break;
             case 'q':
-                var low = buffer.readInt32LE(offset);
+                var low = data.getInt32(offset,true);
                 offset+=4;
-                var n = buffer.readInt32LE(offset) * 4294967296.0 + low;
+                //data.setUint32(offset,true);
+                var n = data.getInt32(offset,true) * 4294967296.0 + low;
                 if (low < 0) n += 4294967296;
                 dict[column[i]]=n;
                 offset+=4;
                 break;
             case 'n':
-                dict[column[i]]=buffer.toString('ascii', offset, offset + 4).replace(/\x00+$/g, '');
+                dict[column[i]]=String.fromCharCode.apply(null, new Uint8Array(buffer,offset,4)).replace(/\x00+$/g, '');
                 offset+=4;
                 break;
             case 'N':
-                dict[column[i]]=buffer.toString('ascii', offset, offset + 16).replace(/\x00+$/g, '');
+                dict[column[i]]=String.fromCharCode.apply(null, new Uint8Array(buffer,offset,16)).replace(/\x00+$/g, '');
                 offset+=16;
                 break;
             case 'Z':
-                dict[column[i]]=buffer.toString('ascii', offset, offset + 64).replace(/\x00+$/g, '');
+                dict[column[i]]=String.fromCharCode.apply(null, new Uint8Array(buffer,offset,64)).replace(/\x00+$/g, '');
                 offset+=64;
                 break;
             case 'c':
-                dict[column[i]]=buffer.readInt16LE(offset)*100;
+                //data.setInt16(offset,true);
+                dict[column[i]]=data.getInt16(offset,true)*100;
                 offset+=2;
                 break;
             case 'C':
-                dict[column[i]]=buffer.readUInt16LE(offset)*100;
+                //data.setUint16(offset,true);
+                dict[column[i]]=data.getUint16(offset,true)*100;
                 offset+=2;
                 break;
             case 'E':
-                dict[column[i]]=buffer.readUInt32LE(offset)*100;
+                //data.setUint32(offset,true);
+                dict[column[i]]=data.getUint32(offset,true)*100;
                 offset+=4;
                 break;
             case 'e':
-                dict[column[i]]=buffer.readInt32LE(offset)*100;
+                //data.setInt32(offset,true);
+                dict[column[i]]=data.getInt32(offset,true)*100;
                 offset+=4;
                 break;
             case 'L':
-                dict[column[i]]=buffer.readInt32LE(offset);
+                //data.setInt32(offset,true);
+                dict[column[i]]=data.getInt32(offset,true);
                 offset+=4;
-                break;
-            case 'i':
-                dict[column[i]]=buffer.readUInt8(offset);
-                offset+=1;
                 break;
         }
     }
@@ -110,13 +113,16 @@ function FORMAT_TO_STRUCT(obj)
 }
 
 function parse_atOffset(type){
+     var count=0;
     for(var i=0;i<msgType.length;i++)
     {
         if(msgType[i]==type) {
+            count++;
             offset = offsetArray[i];
             console.log(FORMAT_TO_STRUCT(FMT[msgType[i]]));
         }
     }
+    console.log(count);
 }
 
 function time_stamp(TimeUs){
@@ -133,38 +139,26 @@ function assign_column(obj){
     return ArrayOfString;
 }
 
-
 function DF_reader()
 {
     while(!buffer.size) {
         offset += 2;
-        var attribute = buffer.readUInt8(offset,offset+1);
+        var attribute = data.getUint8(offset);
         offset += 1;
         offsetArray.push(offset);
         msgType.push(attribute);
         if(FMT[attribute]!=null) {
-            try {
-                var value=FORMAT_TO_STRUCT(FMT[attribute]);
+                var value = FORMAT_TO_STRUCT(FMT[attribute]);
                 if (attribute == '128') {
                     FMT[value['Type']] = {
                         'Type': value['Type'],
-                        'length': value['length'],
+                        'length': value['Length'],
                         'Name': value['Name'],
                         'Format': value['Format'],
                         'Columns': value['Columns']
                     };
                 }
-            }
-            catch(err){
-                //console.log(err.message);
-                break;
-            }
-            finally{
-            }
         }
+        else break;
     }
-    //console.log(toGraph);
 }
-
-DF_reader();
-parse_atOffset(174);
